2e50ee2a430f680513ca5c1fcfc1cadc6e417369
angelavirginmarcella <117150840+angelavirginmarcella@users.noreply.github.com>
2023-12-06 21:26:25 +0700

    Add files via upload

---
+++ web1/pemrograman1/tampil.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+
+</head>
+<body>
+    <h2>TAMPILAN FORM</h2>
+    Nama:<?php echo $_POST["nama"];?>
+    Eamil:<?php echo $_POST["email"];?>
+    Alamat:<?php echo $_POST["alamat"];?>
+</body>
+</html>
